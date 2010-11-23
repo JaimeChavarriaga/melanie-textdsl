c418b6eb6b9b3bbcbb64af2f2187c48662293a36
user <user@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2010-11-23 11:33:29 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@38 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- uni-mannheim.informatik.swt.models.plm.diagram/src/uni_mannheim/informatik/swt/models/plm/PLM/diagram/edit/helpers/PNodeEditHelper.java
+++
@@ -1,9 +0,0 @@
-package uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class PNodeEditHelper
-		extends
-		uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.helpers.PLMBaseEditHelper {
-}
--- uni-mannheim.informatik.swt.models.plm.diagram/src/uni_mannheim/informatik/swt/models/plm/PLM/diagram/edit/helpers/PRelationshipEditHelper.java
+++
@@ -1,9 +0,0 @@
-package uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class PRelationshipEditHelper
-		extends
-		uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.helpers.PLMBaseEditHelper {
-}
