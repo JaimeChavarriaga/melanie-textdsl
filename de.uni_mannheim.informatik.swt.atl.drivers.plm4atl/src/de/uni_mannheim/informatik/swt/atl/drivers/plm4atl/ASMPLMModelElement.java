ab87d949eca84daa891e2857a0f07c69933d268b
rgerbig <rgerbig@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-06-30 09:37:38 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@567 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.atl.drivers.plm4atl/src/de/uni_mannheim/informatik/swt/atl/drivers/plm4atl/ASMPLMModelElement.java
+++ de.uni_mannheim.informatik.swt.atl.drivers.plm4atl/src/de/uni_mannheim/informatik/swt/atl/drivers/plm4atl/ASMPLMModelElement.java
@@ -23,6 +23,7 @@ import org.eclipse.m2m.atl.engine.vm.nativelib.ASMModelElement;
 import org.eclipse.m2m.atl.engine.vm.nativelib.ASMOclAny;
 import org.eclipse.m2m.atl.engine.vm.nativelib.ASMOclSimpleType;
 import org.eclipse.m2m.atl.engine.vm.nativelib.ASMOclType;
+import org.eclipse.m2m.atl.engine.vm.nativelib.ASMOclUndefined;
 import org.eclipse.m2m.atl.engine.vm.nativelib.ASMString;
 import org.eclipse.ocl.ParserException;
 import org.eclipse.ocl.ecore.Constraint;
@@ -36,6 +37,8 @@ import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
 import de.uni_mannheim.informatik.swt.models.plm.PLM.DomainElement;
 import de.uni_mannheim.informatik.swt.models.plm.PLM.Element;
 import de.uni_mannheim.informatik.swt.models.plm.PLM.Feature;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Model;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.util.PLMAdapterFactory;
 
 public class ASMPLMModelElement extends ASMEMFModelElement {
 
@@ -47,8 +50,66 @@ public class ASMPLMModelElement extends ASMEMFModelElement {
 	
 	@Override
 	public ASMOclAny getHelper(StackFrame frame, String name) {
-		// TODO Auto-generated method stub
-		return super.getHelper(frame, name);
+		Boolean cachedOntologicalMode = ontologicalMode;
+		
+		//We need to do this as the implementation calls getType
+		//which will give a wrong result if helper is defined on
+		//ontological type.
+		ontologicalMode = false;
+		ASMOclAny h = super.getHelper(frame, name);
+		
+		if (h instanceof ASMOclUndefined)
+		{
+			ontologicalMode = true;
+			h = super.getHelper(frame, name);
+			ontologicalMode = cachedOntologicalMode;
+		}
+		
+		return h;
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public ASMOclType getType() {
+		
+		ASMOclType t = null;
+		
+		if (ontologicalMode == null || !ontologicalMode)
+			 t =  super.getType();
+		else
+			t = getOntologicalType();
+		return t;
+	}
+	
+	/**
+	 * Retrieves the ontological type from the !meta-model!
+	 * 
+	 * @param frame
+	 * @return
+	 */
+	private ASMModelElement getOntologicalType(){
+		Clabject[] type = null;
+		OCL ocl = OCL.newInstance();
+		OCLHelper<EClassifier, ?, ?, Constraint> helper = ocl
+				.createOCLHelper();
+		OCLExpression<EClassifier> q;
+		
+		helper.setContext(de.uni_mannheim.informatik.swt.models.plm.PLM.PLMPackage.Literals.CLABJECT);
+		
+		try {
+			q = helper.createQuery("Instantiation.allInstances()->select(i | i.instance = self).type->asSequence()");
+		
+			type = ((ArrayList<Clabject>) ocl.evaluate(object, q)).toArray(new Clabject[] {});
+			String qn = ((Model)type[0].eContainer()).getName() + "::" + type[0].getName();
+			return ((ASMPLMModel)PLMModelLoader.eInstance.getLoadedModels().get("PLM")).findModelElement(qn);
+		}
+		catch (ParserException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 	
 	//Have to override this as we are not only looking for helpers on linguistic
@@ -60,24 +121,7 @@ public class ASMPLMModelElement extends ASMEMFModelElement {
 		//linguistic helper not found -> look for ontological one
 		if (!isHelper)
 		{
-			Clabject[] type = null;
-			OCL ocl = OCL.newInstance();
-			OCLHelper<EClassifier, ?, ?, Constraint> helper = ocl
-					.createOCLHelper();
-			OCLExpression<EClassifier> q;
-			
-			helper.setContext(de.uni_mannheim.informatik.swt.models.plm.PLM.PLMPackage.Literals.CLABJECT);
-			
-			try {
-				q = helper.createQuery("Instantiation.allInstances()->select(i | i.instance = self).type->asSequence()");
-			
-				type = ((ArrayList<Clabject>) ocl.evaluate(object, q)).toArray(new Clabject[] {});
-				isHelper = ((ASMExecEnv)frame.getExecEnv()).isHelper((ASMOclType)emf2ASM(frame, type[0]), name);
-			} catch (ParserException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+			isHelper = ((ASMExecEnv)frame.getExecEnv()).isHelper(getOntologicalType(), name);	
 		}
 		return isHelper;
 	}
@@ -87,21 +131,21 @@ public class ASMPLMModelElement extends ASMEMFModelElement {
 		
 		if ("_o_".equals(name))
 		{
-			ontologicAttribute = true;
+			ontologicalMode = true;
 			return this;
 		}
 		else if ("_l_".equals(name))
 		{
-			ontologicAttribute = false;
+			ontologicalMode = false;
 			return this;
 		}
 		else if ((frame != null) && isHelper(frame, name))
 			return getHelper(frame, name);
-		else if (ontologicAttribute == null)
+		else if (ontologicalMode == null)
 		{
 			throw new VMException(frame, "Neither linguistic (_l_) nor ontological (_o_) dimension were selected!", null);
 		}
-		else if (ontologicAttribute)
+		else if (ontologicalMode)
 		{
 			Element[] feature = null;
 			
@@ -135,15 +179,17 @@ public class ASMPLMModelElement extends ASMEMFModelElement {
 				throw new VMException(frame, "Feature " + name + " does not exist on ontologic type " + getType(), null);
 			}
 			
-			ontologicAttribute = null;
+			ontologicalMode = null;
 		 	return feature[0] instanceof Attribute? emf2ASM(frame, ((Attribute)feature[0]).getValue()) : emf2ASM(frame, feature[0]);
 		}
 		else
 		{
-			ontologicAttribute = null;
+			ontologicalMode = null;
 			return super.get(frame, name);
 		}
 	}
 	
-	private Boolean ontologicAttribute = null;
+	//Specifies wheter the model element is in ontologic mode
+	//can be explicitly controlled via _o_ and _l_
+	private Boolean ontologicalMode = null;
 }
