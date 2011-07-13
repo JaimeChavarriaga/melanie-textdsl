28fecc5e2348696f4f2bdb6a99ba0fba73a84c93
rgerbig <rgerbig@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-07-13 09:30:36 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@659 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ de.uni_mannheim.informatik.swt.models.plm.diagram.custom/src/de/uni_mannheim/informatik/swt/models/plm/diagram/custom/DSL.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/
+
+package de.uni_mannheim.informatik.swt.models.plm.diagram.custom;
+
+import java.util.LinkedList;
+
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderedShapeEditPart;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Element;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.PLMPackage;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.parts.BinaryGeneralizationEditPart;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.parts.ConnectionEditPart;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.parts.EntityEditPart;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.parts.MultipleGeneralizationEditPart;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.diagram.edit.parts.MultipleSpecializationEditPart;
+
+
+/**
+ * 
+ * @author Ralph Gerbig (rgerbig@rumms.uni-mannheim.de)
+ *
+ *
+ */
+public class DSL implements IObjectActionDelegate {
+
+	public final static String ID = "de.uni_mannheim.informatik.swt.models.plm.diagram.custom.toggledomainconnectionationaction";
+	
+	private EntityEditPart selectedElement;
+	
+	public DSL()  {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Reads the rendering information attached to the ontology. If no information
+	 * is present it will be created. After reading the information of collapsedDomainConnections
+	 * Field it is decided whether the ID of the connection will be removed or added to the
+	 * connection. In the end the visual state of the DomainConnectionEditPart is changed.
+	 */
+	@Override
+	public void run(IAction action) {
+		
+		if (selectedElement == null)
+			return;
+		
+		((EntityEditPart)selectedElement).updateDSL();
+		
+	}	
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		selectedElement = null;
+		if (selection instanceof IStructuredSelection) 
+		{
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			if (structuredSelection.getFirstElement() instanceof EntityEditPart) 
+			{
+				selectedElement = (EntityEditPart) structuredSelection.getFirstElement();
+			}
+		}
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
