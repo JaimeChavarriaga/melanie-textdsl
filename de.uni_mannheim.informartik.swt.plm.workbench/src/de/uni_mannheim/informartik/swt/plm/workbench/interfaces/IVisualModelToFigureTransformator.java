23c750efab2756ea52dbebc238aee0d45240de44
rgerbig <rgerbig@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-07-26 12:15:00 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@760 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ de.uni_mannheim.informartik.swt.plm.workbench/src/de/uni_mannheim/informartik/swt/plm/workbench/interfaces/IVisualModelToFigureTransformator.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/ 
+package de.uni_mannheim.informartik.swt.plm.workbench.interfaces;
+
+import org.eclipse.draw2d.IFigure;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Visualizer;
+
+/**
+ * Interface for a visual model to figure transformator.
+ * The transformator takes a visualizer as input and generates
+ * a renderable figure as output (e.g. a draw2d figure).
+ *
+ */
+public interface IVisualModelToFigureTransformator {
+	
+	/**
+	 * Runs the transformation
+	 * 
+	 * @param v visualizer to translate into a figure
+	 * 
+	 * @return renderable figure
+	 */
+	public IFigure run(Visualizer v);
+	
+}
