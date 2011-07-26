55064e9138a7755b7a8e1ad761cba1d59bead0b9
rgerbig <rgerbig@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-07-26 11:26:54 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@755 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ de.uni_mannheim.informatik.swt.plm.reasoning.service/src/de/uni_mannheim/informatik/swt/plm/reasoning/service/IReasoningService.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/ 
+package de.uni_mannheim.informatik.swt.plm.reasoning.service;
+
+import java.util.List;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Element;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Visualizer;
+
+/**
+ * Interface that must be implemented for all reasoing engines
+ *
+ */
+public interface IReasoningService {
+	
+	/**
+	 * Creates an instance of the clabject c.
+	 * 
+	 * @param type The Clabject to create an instance from
+	 * 
+	 * @return The clabject which is an instance
+	 */
+	public Clabject createInstanceFrom(Clabject type);
+	
+	/**
+	 * Returns all instances of the clabject type.
+	 * 
+	 * @param type The clabject to return all instances of
+	 * 
+	 * @return A list with instances of the clabject
+	 */
+	public List<Clabject> getAllInstancesOf(Clabject type);
+	
+	/**
+	 * Returns all clabjects that inherit from clabject c.
+	 * 
+	 * @param c The clabject from which the sub clabjects are retrieved
+
+	 * @return A list of clabjects that inherit from c
+	 */
+	public List<Clabject> getAllSubClabjectsOf(Clabject c);
+	
+	
+	/**
+	 * Returns all clabjects from which c inherits from
+	 * 
+	 * @param c The clabject to retrieve all super clabjects from
+	 * 
+	 * @return A list of super clabjects
+	 */
+	public List<Clabject> getAllSyperClabjectsOf(Clabject c);
+	
+	/**
+	 * Checks whether a connection can exist between source and target
+	 * 
+	 * @param source Connection source
+	 *
+	 * @param target Connection target
+	 * 
+	 * @return true => Connection can exist; false => else
+	 */
+	public boolean canConnectionExist(Clabject source, Clabject target);
+	
+	/**
+	 * Checks whether child can be added to parent
+	 * 
+	 * @param parent The element child is added to
+	 * 
+	 * @param child The element which is added to parent
+	 * 
+	 * @return true => child can be added; false => else
+	 */
+	public boolean canAddAsChild(Clabject parent, Clabject child);
+	
+	/**
+	 * Returns all types from which instances can be created in e. e can
+	 * be a clabject, model etc.
+	 * 
+	 * @param e Container to get possible types that can be instantiated for
+	 * 
+	 * @return A list with possible types for creating instances
+	 */
+	public List<Clabject> getAllPossibleTypeForParent(Element e);
+	
+	/**
+	 * Checks whether instance is an instance of type
+	 * 
+	 * @param instance
+	 * @param type
+	 * @return
+	 */
+	public boolean isInstanceOf(Clabject instance, Clabject type);
+	
+	/**
+	 * Checks whether type is a type of instance 
+	 * 
+	 * @param type
+	 * @param instance
+	 * @return
+	 */
+	public boolean isTypeOf(Clabject type, Clabject instance);
+}
