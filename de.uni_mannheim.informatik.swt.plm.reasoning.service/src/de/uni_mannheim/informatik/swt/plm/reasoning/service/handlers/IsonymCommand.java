56671d80c30469f85437f4358eaa5fa0faeb08ef
Bastian.Kennel <Bastian.Kennel@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-09-11 20:53:55 +0000

    some more commands and checks
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@971 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ de.uni_mannheim.informatik.swt.plm.reasoning.service/src/de/uni_mannheim/informatik/swt/plm/reasoning/service/handlers/IsonymCommand.java
@@ -0,0 +1,62 @@
+package de.uni_mannheim.informatik.swt.plm.reasoning.service.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.reasoningresult.ReasoningResult.CompositeCheck;
+import de.uni_mannheim.informatik.swt.models.plm.reasoningresult.ReasoningResult.PotencyComparison;
+import de.uni_mannheim.informatik.swt.models.plm.reasoningresult.ReasoningResult.ReasoningResultFactory;
+import de.uni_mannheim.informatik.swt.models.plm.reasoningresult.ReasoningResult.ReasoningResultModel;
+import de.uni_mannheim.informatik.swt.plm.reasoning.service.ReasoningService;
+import de.uni_mannheim.informatik.swt.plm.workbench.interfaces.IReasoningService;
+
+public class IsonymCommand extends AbstractHandler {
+	
+	IReasoningService reasoner = new ReasoningService().Instance();
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Clabject type = (Clabject)event.getParameters().get("type");
+		Clabject instance = (Clabject)event.getParameters().get("instance");
+		ReasoningResultModel resultModel = ReasoningResultFactory.eINSTANCE.createReasoningResultModel();
+		CompositeCheck check = compute(type, instance);
+		resultModel.getCheck().add(check);
+		reasoner.getReasoningHistory().add(resultModel);
+		return check.isResult();
+	}
+	
+	protected CompositeCheck compute(Clabject type, Clabject instance) {
+		return isIsonym(type, instance);
+	}
+	
+	private CompositeCheck isIsonym(Clabject type, Clabject instance) {
+		CompositeCheck check = ReasoningResultFactory.eINSTANCE.createCompositeCheck(instance, type, null);
+		check.setName("IsHyponym");
+		boolean result = true;
+		CompositeCheck propertyConforms = (new PropertyConformsCommand()).compute(type, instance);
+		check.getCheck().add(propertyConforms);
+		if (!propertyConforms.isResult()) {
+			result = false;
+		}
+		CompositeCheck additionalFeatures = (new HasAdditionalPropertiesCommand()).compute(type, instance);
+		check.getCheck().add(additionalFeatures);
+		if (additionalFeatures.isResult()) {
+			result = false;
+		}
+		PotencyComparison potencyCheck = ReasoningResultFactory.eINSTANCE.createPotencyComparison(instance, type, check);
+		potencyCheck.setInstancePotency(instance.getPotency());
+		potencyCheck.setTargetPotency(type.getPotency());
+		if (type.getPotency() != -1) {
+			if (instance.getPotency() == -1) {
+				result = false;
+			} else if (instance.getPotency() + 1 != type.getPotency()){
+				result = false;
+			}
+		}
+		check.setResult(result);
+		return check;
+	}
+
+}
