3b4edaaae35b48ce133e9aa3aedc786b0f88cb73
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-09-13 13:37:22 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1026 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ tags/Release_20110913/de.uni_mannheim.informartik.swt.plm.workbench/src/de/uni_mannheim/informatik/swt/plm/workbench/interfaces/IRefactoringService.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/
+package de.uni_mannheim.informatik.swt.plm.workbench.interfaces;
+
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+/**
+ * Interface for refactoring extension point.
+ *
+ */
+public interface IRefactoringService {
+	
+	/**
+	 * Refactors the feature of the modelElement with the value
+	 * 
+	 * @param modelElement Model element to change value in
+	 * @param feature Feature to change
+	 * @param value Value to set
+	 * @return true -> refactoring worked; false -> else
+	 */
+	public boolean changeValue(EObject modelElement, EStructuralFeature feature, Object value);
+	
+	public boolean delete(EObject modelElement);
+	
+	public Map<String, String> getAvailableRefactoringCommands(EObject modelElement);
+}
