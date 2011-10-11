b4f963032d26c8c4bfc1ed2511a540f5415bb546
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-10-11 10:43:11 +0000

    Before Changing Generalization in MM
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1138 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ tags/Before Changing Generalization in MM/de.uni_mannheim.informartik.swt.plm.workbench/src/de/uni_mannheim/informatik/swt/plm/workbench/interfaces/IVisualModelToFigureTransformator.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/ 
+package de.uni_mannheim.informatik.swt.plm.workbench.interfaces;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.swt.graphics.Color;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Visualizer;
+import de.uni_mannheim.informatik.swt.models.plm.visualization.ColorConstant;
+
+/**
+ * Interface for a visual model to figure transformator.
+ * The transformator takes a visualizer as input and generates
+ * a renderable figure as output (e.g. a draw2d figure).
+ *
+ */
+public interface IVisualModelToFigureTransformator {
+	
+	/**
+	 * Runs the transformation
+	 * 
+	 * @param v visualizer to translate into a figure
+	 * 
+	 * @return renderable figure
+	 */
+	public IFigure run(Visualizer v);
+	
+	/**
+	 * Translates a color constant into a SWT Color
+	 * 
+	 * @param constant constant to translate
+	 * @return SWT Color
+	 */
+	public Color colorConstant2Color(ColorConstant constant);
+}
