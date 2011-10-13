dd02e888bdcd01d529e12374c8fc499f266eae08
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-10-13 14:04:43 +0000

    Implemented DeleteFeatureCommand
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1161 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.plm.refactoring.service/src/de/uni_mannheim/informatik/swt/plm/refactoring/service/handlers/DeleteFeatureCommand.java
+++ de.uni_mannheim.informatik.swt.plm.refactoring.service/src/de/uni_mannheim/informatik/swt/plm/refactoring/service/handlers/DeleteFeatureCommand.java
@@ -1,11 +1,25 @@
 package de.uni_mannheim.informatik.swt.plm.refactoring.service.handlers;
 
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.edit.command.RemoveCommand;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Feature;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.PLMPackage;
+import de.uni_mannheim.informatik.swt.plm.workbench.ExtensionPointService;
+import de.uni_mannheim.informatik.swt.plm.workbench.interfaces.IReasoningService;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -27,11 +41,29 @@ public class DeleteFeatureCommand extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Service",
-				"Delete Feature!");
-		return null;
+		
+		Feature featureToChange = (Feature)event.getObjectParameterForExecution("feature");
+		Clabject containingClabject = (Clabject)featureToChange.eContainer();
+		
+		List<Clabject> instances = containingClabject.getModelInstances();
+		
+		CompoundCommand refactoringCommand = new CompoundCommand("Refactoring");
+		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(featureToChange);
+		
+		for (Clabject instance: instances)
+			for (Feature feature : instance.getAllFeatures())
+				try {
+					if (ExtensionPointService.Instance().getActiveReasoningService().run(IReasoningService.FEATURE_CONFORMS, new Object[] {featureToChange, feature})){
+						refactoringCommand.append(RemoveCommand.create(domain, feature));
+					}
+						
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+		
+		refactoringCommand.append(RemoveCommand.create(domain, featureToChange));
+		domain.getCommandStack().execute(refactoringCommand);
+		
+		return false;
 	}
 }
