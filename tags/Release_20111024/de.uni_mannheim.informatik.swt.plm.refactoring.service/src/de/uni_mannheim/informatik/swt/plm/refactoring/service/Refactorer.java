de0ae5869b41c7fddb143b79b7d7fb4643b11a80
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-10-24 14:55:41 +0000

    Release_20111024
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1246 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ tags/Release_20111024/de.uni_mannheim.informatik.swt.plm.refactoring.service/src/de/uni_mannheim/informatik/swt/plm/refactoring/service/Refactorer.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/
+package de.uni_mannheim.informatik.swt.plm.refactoring.service;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.jface.action.ContributionItem;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.menus.CommandContributionItem;
+import org.eclipse.ui.menus.CommandContributionItemParameter;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Feature;
+import de.uni_mannheim.informatik.swt.plm.refactoring.service.handlers.AddFeatureToClabjectCommand;
+import de.uni_mannheim.informatik.swt.plm.refactoring.service.handlers.DeleteClabjectCommand;
+import de.uni_mannheim.informatik.swt.plm.refactoring.service.handlers.DeleteFeatureCommand;
+import de.uni_mannheim.informatik.swt.plm.refactoring.service.handlers.RenameFeatureCommand;
+import de.uni_mannheim.informatik.swt.plm.workbench.interfaces.IRefactoringService;
+
+
+public class Refactorer implements IRefactoringService {
+	
+	@Override
+	public boolean changeValue(EObject modelElement,
+			EStructuralFeature feature, Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(EObject modelElement) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<ContributionItem> getAvailableRefactoringCommands(EObject[] modelElements){
+		
+		List<ContributionItem> items = new LinkedList<ContributionItem>();
+		Map<String, Object> commandParamametersMap;
+		
+		//We have one feature selected
+		if (modelElements.length == 1
+				&& modelElements[0] instanceof Feature)
+		{
+			//***************************************************
+			// Delete feature command
+			//***************************************************
+			CommandContributionItemParameter param = 
+					new CommandContributionItemParameter(PlatformUI.getWorkbench().getActiveWorkbenchWindow(), DeleteFeatureCommand.ID + ".menuEntry", DeleteFeatureCommand.ID, CommandContributionItem.STYLE_PUSH);
+			param.label = getCommandName(DeleteFeatureCommand.ID);
+			
+			commandParamametersMap = new HashMap<String, Object>();
+			commandParamametersMap.put("feature",  modelElements[0]);
+			param.parameters = commandParamametersMap;
+			
+			items.add(new CommandContributionItem(param));
+			
+			//***************************************************
+			// Rename feature command
+			//***************************************************
+			param = 
+					new CommandContributionItemParameter(PlatformUI.getWorkbench().getActiveWorkbenchWindow(), RenameFeatureCommand.ID + ".menuEntry", RenameFeatureCommand.ID, CommandContributionItem.STYLE_PUSH);
+			param.label = getCommandName(RenameFeatureCommand.ID);
+			
+			commandParamametersMap = new HashMap<String, Object>();
+			commandParamametersMap.put("feature",  modelElements[0]);
+			param.parameters = commandParamametersMap;
+			
+			items.add(new CommandContributionItem(param));
+		}
+		//We have one feature selected
+		if (modelElements.length == 1
+				&& modelElements[0] instanceof Clabject)
+		{
+			//***************************************************
+			// Delete clabject command
+			//***************************************************
+			CommandContributionItemParameter param = 
+					new CommandContributionItemParameter(PlatformUI.getWorkbench().getActiveWorkbenchWindow(), DeleteClabjectCommand.ID + ".menuEntry", DeleteClabjectCommand.ID, CommandContributionItem.STYLE_PUSH);
+			param.label = getCommandName(DeleteClabjectCommand.ID);
+			
+			commandParamametersMap = new HashMap<String, Object>();
+			commandParamametersMap.put("clabject",  modelElements[0]);
+			param.parameters = commandParamametersMap;
+			
+			items.add(new CommandContributionItem(param));
+			
+			//***************************************************
+			// Add feature to clabject command
+			//***************************************************
+			param = 
+					new CommandContributionItemParameter(PlatformUI.getWorkbench().getActiveWorkbenchWindow(), AddFeatureToClabjectCommand.ID + ".menuEntry", AddFeatureToClabjectCommand.ID, CommandContributionItem.STYLE_PUSH);
+			param.label = getCommandName(AddFeatureToClabjectCommand.ID);
+			
+			commandParamametersMap = new HashMap<String, Object>();
+			commandParamametersMap.put("clabject",  modelElements[0]);
+			param.parameters = commandParamametersMap;
+			
+			items.add(new CommandContributionItem(param));
+		}
+		
+		return items;
+	}
+	
+	private String getCommandName(String id){
+		
+		//Initialize the visualization service
+		IConfigurationElement[] configurationElements = Platform.getExtensionRegistry().getExtension("de.uni_mannheim.informatik.swt.plm.refactoring.service.commands").getConfigurationElements();
+		
+			for (IConfigurationElement ce : configurationElements)
+				if (ce.getAttribute("id").equalsIgnoreCase(id))
+					return (String)ce.getAttribute("name");
+			
+		return null;
+	}
+
+}
---
+++ tags/Release_20111024/de.uni_mannheim.informatik.swt.plm.refactoring.service/src/de/uni_mannheim/informatik/swt/plm/refactoring/service/handlers/DeleteFeatureCommand.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/
+package de.uni_mannheim.informatik.swt.plm.refactoring.service.handlers;
+
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.edit.command.RemoveCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Feature;
+import de.uni_mannheim.informatik.swt.plm.workbench.ExtensionPointService;
+import de.uni_mannheim.informatik.swt.plm.workbench.interfaces.IReasoningService;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class DeleteFeatureCommand extends AbstractHandler {
+	
+	public final static String ID = "de.uni_mannheim.informatik.swt.plm.refactoring.service.commands.deletefeaturecommand";
+	
+	/**
+	 * The constructor.
+	 */
+	public DeleteFeatureCommand() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		Feature featureToChange = (Feature)event.getObjectParameterForExecution("feature");
+		Clabject containingClabject = (Clabject)featureToChange.eContainer();
+		
+		List<Clabject> instances = containingClabject.getModelInstances();
+		
+		CompoundCommand refactoringCommand = new CompoundCommand("Refactoring");
+		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(featureToChange);
+		
+		for (Clabject instance: instances)
+			for (Feature feature : instance.getFeature())
+				try {
+					if (ExtensionPointService.Instance().getActiveReasoningService().run(IReasoningService.FEATURE_CONFORMS, new Object[] {featureToChange, feature}, true)){
+						refactoringCommand.append(RemoveCommand.create(domain, feature));
+					}
+						
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+		
+		refactoringCommand.append(RemoveCommand.create(domain, featureToChange));
+		domain.getCommandStack().execute(refactoringCommand);
+		
+		return false;
+	}
+}
