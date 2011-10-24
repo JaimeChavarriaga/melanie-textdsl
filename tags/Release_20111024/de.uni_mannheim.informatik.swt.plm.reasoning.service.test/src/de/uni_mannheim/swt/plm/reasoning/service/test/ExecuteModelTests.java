40b20fa4859cfb464f57a8fd4b3c05f7eba0eb4c
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-10-24 14:54:18 +0000

    Release_20111024
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1235 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ tags/Release_20111024/de.uni_mannheim.informatik.swt.plm.reasoning.service.test/src/de/uni_mannheim/swt/plm/reasoning/service/test/ExecuteModelTests.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig, Bastian Kennel 
+ *    				  - initial API and implementation and initial documentation
+ *******************************************************************************/ 
+package de.uni_mannheim.swt.plm.reasoning.service.test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Attribute;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Connection;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Element;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Generalization;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Classification;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Model;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Ontology;
+import de.uni_mannheim.informatik.swt.plm.workbench.ExtensionPointService;
+import de.uni_mannheim.informatik.swt.plm.workbench.interfaces.IReasoningService;
+
+/**
+ * @author bastian
+ *
+ */
+public class ExecuteModelTests implements IObjectActionDelegate {
+
+	private List<Element> selectedElements = new LinkedList<Element>();
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	@Override
+	public void run(IAction action) {
+		try
+		{
+			if (selectedElements.size() > 0) {
+				Element e = (Element) selectedElements.get(0);
+				IReasoningService reasoner = ((IReasoningService )ExtensionPointService.Instance().getActiveReasoningService()).Instance();
+				if(e instanceof Clabject) {
+					
+					if (selectedElements.size() > 1) {
+						Clabject type = (Clabject) selectedElements.get(0);
+						Clabject instance = (Clabject) selectedElements.get(1);
+						System.out.println("isInstance " + reasoner.run(IReasoningService.IS_INSTANCE, new Object[]{type, instance}));
+						if (type instanceof Connection) {
+							System.out.println("Local Construction Conformance " + reasoner.localConstructionConformsConnection((Connection) type, (Connection) instance));
+							System.out.println("Neighbour Construction Conformance " + reasoner.neighbourhoodConstructionConformsConnection((Connection) type, (Connection) instance) );
+						} 
+					} else {
+						Clabject c = (Clabject) e;
+						System.out.println("Model Supertypes" + c.getModelSupertypes());
+						System.out.println("Model Subtypes" + c.getModelSubtypes());
+						System.out.println("Model Instances" + c.getModelInstances());
+						System.out.println("Model Types" + c.getModelTypes());
+						if (e instanceof Connection) {
+							System.out.println("Multiplicity conformance " + reasoner.run(IReasoningService.MULTIPLICITY_CONFORMS, new Object[]{e}));
+						}
+					} 
+				} else if (e instanceof Attribute) {
+					Attribute type = (Attribute) e;
+					Attribute instance = (Attribute) selectedElements.get(1);
+					
+					System.out.println("" + instance + ".conforms("+type+")" + reasoner.run(IReasoningService.FEATURE_CONFORMS, new Object[]{type, instance}));
+				} else if (e instanceof Generalization) {
+						System.out.println("Logical Element Consistency " + reasoner.run(IReasoningService.GENERALIZATION_CONSISTENCY, new Object[]{e}));
+				} else if (e instanceof Classification){
+					System.out.println("Logical Element Consistency " + reasoner.run(IReasoningService.CLASSIFICATION_CONSISTENCY, new Object[]{e}));
+				}
+				else if(e instanceof Model) {
+					System.out.println("Model Consistent Classification " + reasoner.run(IReasoningService.IS_CONSISTENTLY_CLASSIFIED, new Object[]{e}));
+				} else if (e instanceof Ontology) {
+					System.out.println("Ontology Consistency " + reasoner.run(IReasoningService.ONTOLOGY_CONSISTENCY, new Object[]{e}));
+				}
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	@Override
+	public void selectionChanged(IAction action, ISelection selection)
+	{	
+		selectedElements.clear();
+		
+		if (selection instanceof IStructuredSelection)
+		{
+			Iterator i = ((IStructuredSelection) selection).iterator();
+			
+			while (i.hasNext())
+			{
+				Object obj = i.next();
+				
+				if (obj instanceof IGraphicalEditPart && ((IGraphicalEditPart)obj).resolveSemanticElement() instanceof Element)
+					selectedElements.add((Element)((IGraphicalEditPart)obj).resolveSemanticElement());
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction, org.eclipse.ui.IWorkbenchPart)
+	 */
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
