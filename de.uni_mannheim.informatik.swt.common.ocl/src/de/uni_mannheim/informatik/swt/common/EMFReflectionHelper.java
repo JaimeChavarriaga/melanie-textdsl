94527bc4bf585dfd207e31d2a335e777b5814ed7
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2011-12-19 16:09:47 +0000

    Extension by getFeature
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1396 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.common.ocl/src/de/uni_mannheim/informatik/swt/common/EMFReflectionHelper.java
+++ de.uni_mannheim.informatik.swt.common.ocl/src/de/uni_mannheim/informatik/swt/common/EMFReflectionHelper.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.ocl.ParserException;
 
 public class EMFReflectionHelper {
@@ -14,4 +15,8 @@ public class EMFReflectionHelper {
 		return context.eInvoke(opration, new BasicEList<>());
 	}
 	
+	public static Object getFeature(EObject context, String name) throws ParserException{
+		EStructuralFeature feature = (EStructuralFeature)OCLHelper.execute(context.eClass(), "self.eAllStructuralFeatures->select(o | o.name = '" + name + "')->first()");
+		return context.eGet(feature);
+	}
 }
