6f491f18367e0bcaf6e86bd8df356bbf55dcad0a
Bastian.Kennel <Bastian.Kennel@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2012-01-03 14:20:36 +0000

    uff jeden fall
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1417 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.plm.reasoning.service/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/proximity/ProximityIndicator.java
+++ de.uni_mannheim.informatik.swt.plm.reasoning.service/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/proximity/ProximityIndicator.java
@@ -16,7 +16,11 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.emf.ecore.EObject;
+
 import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Element;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.LMLModel;
 
 /**
  * @author bastian
@@ -24,34 +28,112 @@ import de.uni_mannheim.informatik.swt.models.plm.PLM.Clabject;
  */
 public class ProximityIndicator {
 	
+	private interface ProximityHelper<T> {
+		
+		public String appendResult(String currentResult, String newAddition);
+		
+		public List<T> getNextProximityLevel(T current);
+		
+		public String getSeparator();
+		
+	}
+	
 	private LinkedList<Integer> generQueue = new LinkedList<Integer>();
 	private LinkedList<Integer> contQueue = new LinkedList<Integer>();
 	private LinkedList<Integer> classQueue = new LinkedList<Integer>();
+	
+	private final int STAR = -1;
 
 	public String run(Clabject c, String expression) {
 		parseInput(expression);
+		String generalizationProximityString = createProximityString(c, generQueue, new ProximityHelper<Clabject>(){
+
+			@Override
+			public String appendResult(String currentResult, String newAddition) {
+				return newAddition + currentResult;
+			}
+
+			@Override
+			public List<Clabject> getNextProximityLevel(Clabject current) {
+				return current.getModelDirectSupertypes();
+			}
+
+			@Override
+			public String getSeparator() {
+				return "<";
+			}
+			
+		});
+		String classificationProximityString = createProximityString(c, classQueue, new ProximityHelper<Clabject>() {
+
+			@Override
+			public String appendResult(String currentResult, String newAddition) {
+				return currentResult + newAddition;
+			}
+
+			@Override
+			public List<Clabject> getNextProximityLevel(Clabject current) {
+				return current.getModelDirectTypes();
+			}
+
+			@Override
+			public String getSeparator() {
+				return ":";
+			}
+			
+		});
+		String containmentProximityString = createProximityString(c, contQueue, new ProximityHelper<Element>() {
+
+			@Override
+			public String appendResult(String currentResult, String newAddition) {
+				return newAddition + currentResult;
+			}
+
+			@Override
+			public List<Element> getNextProximityLevel(Element current) {
+				ArrayList<Element> temp = new ArrayList<Element>();
+				EObject possible = current.eContainer();
+				if (possible instanceof LMLModel) return temp;
+				temp.add((Element) possible);
+				return temp;
+			}
+
+			@Override
+			public String getSeparator() {
+				return ".";
+			}
+			
+		});
+		String result = generalizationProximityString + containmentProximityString + c.getName() + classificationProximityString;  
+		return  result;
+	}
+
+
+	private String createProximityString(Clabject c, LinkedList<Integer> queue, ProximityHelper helper) {
 		String result = "";
-		if (generQueue.isEmpty()) {
+		if (queue.isEmpty()) {
 			return result;
 		}
-		int maxLevel = generQueue.peekLast();
-		int currentRequest = generQueue.poll();
+		int currentRequest = queue.poll();
 		int currentLevel = 0;
-		List<List<Clabject>> allLevels = new ArrayList<List<Clabject>>();
-		allLevels.add(new ArrayList<Clabject>(c.getModelDirectSupertypes()));
-		while (currentLevel <= maxLevel) {
-			List<Clabject> nextLevel = new ArrayList<Clabject>();
-			for (Clabject t: allLevels.get(currentLevel)) {
-				nextLevel.addAll(t.getModelDirectSupertypes());
+		int lastRequest = -1;
+		List<List<? extends Element>> allLevels = new ArrayList<List<? extends Element>>();
+		allLevels.add(helper.getNextProximityLevel(c));
+		boolean nextLevelPresent = true;
+		while (nextLevelPresent) {
+			List<? extends Element> nextLevel = new ArrayList<Element>();
+			for (Element t: allLevels.get(currentLevel)) {
+				nextLevel.addAll(helper.getNextProximityLevel(t));
 			}
-			allLevels.add(nextLevel);
+			nextLevelPresent = nextLevel.size() > 0;
+			if (nextLevelPresent)	allLevels.add(nextLevel);
 			currentLevel++;
 		}
-		for (List<Clabject> currentLevelSupertypes: allLevels) {
-			if (currentRequest == allLevels.indexOf(currentLevelSupertypes)) { 
+		for (List<? extends Element> currentLevelClabjects: allLevels) {
+			if (currentRequest == allLevels.indexOf(currentLevelClabjects) || currentRequest == STAR) { 
 				String temp = "";
-				if (currentLevelSupertypes.size()>1) temp += "(";
-				for (Clabject t: currentLevelSupertypes) {
+				if (currentLevelClabjects.size()>1) temp += "(";
+				for (Element t: currentLevelClabjects) {
 					if (t.getName() != null && t.getName().length()>0) {
 						temp += t.getName();
 					} else {
@@ -59,17 +141,20 @@ public class ProximityIndicator {
 					}
 					temp += ",";
 				}
-				temp = temp.substring(0,temp.length()-1);
-				if (currentLevelSupertypes.size()>1) temp += ")";
-				temp += "<";
-				result += temp;
-				if (!generQueue.isEmpty()) {
-					currentRequest = generQueue.poll();
+				if (temp.length() > 0) temp = temp.substring(0,temp.length()-1);
+				if (currentLevelClabjects.size()>1) temp += ")";
+				temp += helper.getSeparator();
+				if (currentRequest != STAR && currentRequest - lastRequest >= 2) temp += helper.getSeparator();
+				result = helper.appendResult(result, temp);
+				if (!queue.isEmpty()) {
+					lastRequest = currentRequest;
+					currentRequest = queue.poll();
 				}
 			}
 		}
 		return result;
 	}
+	
 
 	private void parseInput(String expression) {
 		Pattern pattern = Pattern.compile("\\[(.*?)\\]\\((.*?)\\)\\{(.*?)\\}");
