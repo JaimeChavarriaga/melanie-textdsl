d9b7b1d06cd8333a1fcede3d9e5ac64251f98647
bastian.kennel <bastian.kennel@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2012-05-07 13:24:39 +0000

    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1723 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ de.uni_mannheim.informatik.swt.mlm.reasoning.service/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/handlers/GeneralizationRealizationCommand.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bastian Kennel - initial API and implementation and initial documentation
+ *    Ralph Gerbig - non reasoning related programming
+ *******************************************************************************/
+package de.uni_mannheim.informatik.swt.mlm.reasoning.service.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import de.uni_mannheim.informatik.swt.mlm.reasoning.service.ReasoningService;
+import de.uni_mannheim.informatik.swt.mlm.reasoning.service.util.ReasoningServiceUtil;
+import de.uni_mannheim.informatik.swt.mlm.workbench.interfaces.IReasoningService;
+import de.uni_mannheim.informatik.swt.models.plm.PLM.Generalization;
+import de.uni_mannheim.informatik.swt.models.reasoningresult.ReasoningResult.Check;
+import de.uni_mannheim.informatik.swt.models.reasoningresult.ReasoningResult.ReasoningResultFactory;
+import de.uni_mannheim.informatik.swt.models.reasoningresult.ReasoningResult.ReasoningResultModel;
+
+public class GeneralizationRealizationCommand extends AbstractHandler {
+	
+	public static final String ID_GENER_REALIZATION = "de.uni_mannheim.informatik.swt.plm.reasoning.service.commands.generalizationrealizationcommand";
+	IReasoningService reasoner = (new ReasoningService()).Instance();
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ReasoningResultModel resultModel = ReasoningResultFactory.eINSTANCE.createReasoningResultModel();
+		resultModel.setName("Subsumtion " + ReasoningServiceUtil.getDateString());
+		Check check = null;
+		Generalization gener = (Generalization) event.getObjectParameterForExecution("generalization");  
+		check = compute(gener);
+		resultModel.getChildren().add(check);		
+		
+		Boolean silent = event.getParameters().get("silent") == null?
+				false: Boolean.parseBoolean(event.getParameters().get("silent").toString());
+		if (!silent)
+			reasoner.getReasoningHistory().add(resultModel);
+		return check.isResult();
+	}
+
+	private Check compute(Generalization gener) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
