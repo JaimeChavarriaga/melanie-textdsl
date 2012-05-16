eb22b6af8368b06835f481cc34e28e2f69a8c395
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2012-05-16 10:52:35 +0000

    Fixed copyright statement
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1771 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.mlm.reasoning.service/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/handlers/SubsumptionCommand.java
+++ de.uni_mannheim.informatik.swt.mlm.reasoning.service/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/handlers/SubsumptionCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 University of Mannheim: Chair for Software Engineering
+ * Copyright (c) 2012 University of Mannheim: Chair for Software Engineering
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *    Bastian Kennel - initial API and implementation and initial documentation
- *    Ralph Gerbig - non reasoning related programming
  *******************************************************************************/
 package de.uni_mannheim.informatik.swt.mlm.reasoning.service.handlers;
 
