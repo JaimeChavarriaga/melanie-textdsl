839b75095b200c6d69b73bc4a640fcfb73b64a68
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2012-07-02 12:12:27 +0000

    Auto layout service does contribute auto layout pop up bar button to connecting model elements.
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1845 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

---
+++ de.uni_mannheim.informatik.swt.mlm.autolayout/src/de/uni_mannheim/informatik/swt/mlm/autolayout/AutoLayoutService.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2012 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/
+package de.uni_mannheim.informatik.swt.mlm.autolayout;
+
+import org.eclipse.draw2d.geometry.Rectangle;
+
+import de.uni_mannheim.informatik.swt.mlm.workbench.interfaces.IAutoLayoutService;
+
+public class AutoLayoutService implements IAutoLayoutService {
+
+	@Override
+	public Rectangle doLayout(Rectangle[] clabjectClientRectangles) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
