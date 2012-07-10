88a99fde6296c536c793a4d8a381c3958492f1a8
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2012-07-10 16:06:57 +0000

    Reasoning UI can now expand collapse distinct parts.
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1872 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.mlm.reasoning.service.view/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/view/views/ReasoningView.java
+++ de.uni_mannheim.informatik.swt.mlm.reasoning.service.view/src/de/uni_mannheim/informatik/swt/mlm/reasoning/service/view/views/ReasoningView.java
@@ -45,14 +45,21 @@ import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.ExpandEvent;
+import org.eclipse.swt.events.ExpandListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.ExpandBar;
+import org.eclipse.swt.widgets.ExpandItem;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
@@ -82,6 +89,9 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 	private Element reasoningTarget = null;
 	private Text reasoningTargetText = null;
 	private Button runButton = null;
+	
+	ExpandItem reasoningSelectionExpandItem = null;
+	ExpandItem reasoningResultExpandItem = null;
 	
 	ComboViewer reasoningActionComboViewer = null;
 	
@@ -126,7 +136,7 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 	 * This is a callback that will allow us
 	 * to create the viewer and initialize it.
 	 */
-	public void createPartControl(Composite parent) {
+	public void createPartControl(final Composite parent) {
 		
 		parent.setLayout(new FillLayout());
 		
@@ -138,75 +148,70 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 		//****************************************
 		CTabItem reasoningResultPage = new CTabItem(tabFolder, SWT.NONE);
 		reasoningResultPage.setText("Reasoning Results");
+		
 		Composite reasoningResultComposite = new Composite(tabFolder, SWT.NONE);
 		reasoningResultComposite.setLayout(new GridLayout(1, false));
 		reasoningResultPage.setControl(reasoningResultComposite);
 		
-		GridData reasoningSelectionGroupGridData = new GridData();
-		reasoningSelectionGroupGridData.grabExcessHorizontalSpace = true;
-		reasoningSelectionGroupGridData.horizontalAlignment = SWT.FILL;
+		GridData expandBarGridData = new GridData();
+		expandBarGridData.grabExcessHorizontalSpace = true;
+		expandBarGridData.grabExcessVerticalSpace = true;
+		expandBarGridData.horizontalAlignment = SWT.FILL;
+		expandBarGridData.verticalAlignment = SWT.FILL;
 		
-		Group reasoningSelectionGroup = new Group(reasoningResultComposite, SWT.SHADOW_ETCHED_IN);
-		reasoningSelectionGroup.setText("Reasoning Selection");
-		reasoningSelectionGroup.setLayoutData(reasoningSelectionGroupGridData);
-		reasoningSelectionGroup.setLayout(new GridLayout(1, false));
+		final ExpandBar bar = new ExpandBar(reasoningResultComposite, SWT.V_SCROLL);
+		bar.setSpacing(4);
+		bar.setLayoutData(expandBarGridData);
+		bar.setBackground(parent.getBackground());
+		
+		Composite reasoningResultSelectionComposite = new Composite(bar, SWT.NONE);
+		reasoningResultSelectionComposite.setLayout(new GridLayout(1, false));
+		reasoningResultSelectionComposite.setBackground(parent.getBackground());
 		
 		GridData reasoningSelectionGridData = new GridData();
 		reasoningSelectionGridData.grabExcessHorizontalSpace = true;
 		reasoningSelectionGridData.horizontalAlignment = SWT.FILL;
+		reasoningResultSelectionComposite.setLayoutData(reasoningSelectionGridData);
 		
-		reasoningSourceText = new Text(reasoningSelectionGroup, SWT.SINGLE | SWT.BORDER);
+		reasoningSourceText = new Text(reasoningResultSelectionComposite, SWT.SINGLE | SWT.BORDER);
 		reasoningSourceText.setLayoutData(reasoningSelectionGridData);
 		reasoningSourceText.setEditable(false);
 		reasoningSourceText.setText("<<Source>>");
 		
 		
-		reasoningActionComboViewer = new ComboViewer(reasoningSelectionGroup, SWT.READ_ONLY);
+		reasoningActionComboViewer = new ComboViewer(reasoningResultSelectionComposite, SWT.READ_ONLY);
 		reasoningActionComboViewer.setContentProvider(ArrayContentProvider.getInstance());
 		reasoningActionComboViewer.getCombo().setLayoutData(reasoningSelectionGridData);
 		reasoningActionComboViewer.getCombo().select(0);
 		reasoningActionComboViewer.getCombo().setEnabled(false);
 		
-		reasoningTargetText = new Text(reasoningSelectionGroup, SWT.SINGLE | SWT.BORDER);
+		reasoningTargetText = new Text(reasoningResultSelectionComposite, SWT.SINGLE | SWT.BORDER);
 		reasoningTargetText.setLayoutData(reasoningSelectionGridData);
 		reasoningTargetText.setEditable(false);
 		reasoningTargetText.setText("<<Target>>");
 		
-		runButton = new Button(reasoningSelectionGroup, SWT.NONE);
+		runButton = new Button(reasoningResultSelectionComposite, SWT.NONE);
 		runButton.setEnabled(false);
 		runButton.setText("Run !");
-		runButton.setLayoutData(reasoningSelectionGroupGridData);
-		runButton.addSelectionListener(new SelectionListener() {
-			
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				try {
-					IReasoningService service = ExtensionPointService.Instance().getActiveReasoningService();
-					if (reasoningTarget != null)
-						service.run(name2commandId.get(reasoningActionComboViewer.getCombo().getText()), new EObject[]{reasoningTarget, reasoningSource});
-					else
-						service.run(name2commandId.get(reasoningActionComboViewer.getCombo().getText()), new EObject[]{reasoningSource});
-				} catch (CoreException ex) {
-					ex.printStackTrace();
-				}
-			}
-			
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				//DO NOTHING
-			}
-		});
+		runButton.setLayoutData(expandBarGridData);
 		
-		GridData reasoningResultGroupGridData = new GridData();
-		reasoningResultGroupGridData.grabExcessHorizontalSpace = true;
-		reasoningResultGroupGridData.horizontalAlignment = SWT.FILL;
-		reasoningResultGroupGridData.grabExcessVerticalSpace = true;
-		reasoningResultGroupGridData.verticalAlignment = SWT.FILL;
+		final ExpandItem item0 = new ExpandItem (bar, SWT.NONE, 0);
+		item0.setText("Reasoning Selection");
+		item0.setHeight(reasoningResultSelectionComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		item0.setControl(reasoningResultSelectionComposite);
+		Image image = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay().getSystemImage(SWT.ICON_QUESTION);
+		item0.setImage(image);
+		item0.setExpanded(true);
+		reasoningSelectionExpandItem = item0;
 		
-		Group reasoningResultGroup = new Group(reasoningResultComposite, SWT.SHADOW_ETCHED_IN);
-		reasoningResultGroup.setText("Reasoning Result");
-		reasoningResultGroup.setLayoutData(reasoningResultGroupGridData);
-		reasoningResultGroup.setLayout(new GridLayout(1, false));
+		final Composite reasoningResultResultComposite = new Composite(bar, SWT.NONE);
+		reasoningResultResultComposite.setLayout(new GridLayout(1, false));
+		reasoningResultResultComposite.setBackground(parent.getBackground());
+		
+		GridData reasoningResultResultGridData = new GridData();
+		reasoningResultResultGridData.grabExcessHorizontalSpace = true;
+		reasoningResultResultGridData.horizontalAlignment = SWT.FILL;
+		reasoningResultSelectionComposite.setLayoutData(reasoningResultResultGridData);
 		
 		factory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
 
@@ -224,7 +229,7 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 		factory.addAdapterFactory(new ReasoningResultItemProviderAdapterFactory());
 		factory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
 				
-		comboViewer = new ComboViewer(reasoningResultGroup, SWT.READ_ONLY);
+		comboViewer = new ComboViewer(reasoningResultResultComposite, SWT.READ_ONLY);
 		comboViewer.setContentProvider(ArrayContentProvider.getInstance());
 		comboViewer.setLabelProvider(new AdapterFactoryLabelProvider(factory));
 		comboViewer.getCombo().setLayoutData(comboViewGridData);
@@ -241,7 +246,7 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 			}
 		});
 		
-		treeViewer = new TreeViewer(reasoningResultGroup, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		treeViewer = new TreeViewer(reasoningResultResultComposite, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		treeViewer.getTree().setLayoutData(treeViewGridData);
 		
 		drillDownAdapter = new DrillDownAdapter(treeViewer);
@@ -250,6 +255,75 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 		//viewer.setSorter(new NameSorter());
 		
 		treeViewer.addSelectionChangedListener(menuBuilder);
+		
+		final ExpandItem item1 = new ExpandItem (bar, SWT.NONE, 1);
+		item1.setText("Reasoning Results");
+		//item1.setHeight(reasoningResultResultComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		item1.setControl(reasoningResultResultComposite);
+		Image image1 = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay().getSystemImage(SWT.ICON_INFORMATION);
+		item1.setImage(image1);
+		reasoningResultExpandItem = item1;
+		
+		parent.addListener(SWT.Resize, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				int parentHeight = parent.getSize().y;
+				int compositeStart = reasoningResultResultComposite.getLocation().y;
+				item1.setHeight(parentHeight - compositeStart - item1.getHeaderHeight() - bar.getSpacing() - 4);
+			}
+		});
+		
+		
+		bar.addExpandListener(new ExpandListener() {
+			
+			@Override
+			public void itemExpanded(ExpandEvent e) {
+				if (e.item == item0){
+					int parentHeight = parent.getSize().y;
+					int compositeStart = reasoningResultResultComposite.getLocation().y + ((ExpandItem)e.item).getHeight();
+					item1.setHeight(parentHeight - compositeStart - item1.getHeaderHeight() - bar.getSpacing() - 4);
+				}
+			}
+			
+			@Override
+			public void itemCollapsed(ExpandEvent e) {
+				if (e.item == item0){
+					int parentHeight = parent.getSize().y;
+					int compositeStart = reasoningResultResultComposite.getLocation().y - ((ExpandItem)e.item).getHeight();
+					item1.setHeight(parentHeight - compositeStart - item1.getHeaderHeight() - bar.getSpacing() - 4);
+				}
+			}
+		});
+		
+		runButton.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					IReasoningService service = ExtensionPointService.Instance().getActiveReasoningService();
+					if (reasoningTarget != null)
+						service.run(name2commandId.get(reasoningActionComboViewer.getCombo().getText()), new EObject[]{reasoningTarget, reasoningSource});
+					else
+						service.run(name2commandId.get(reasoningActionComboViewer.getCombo().getText()), new EObject[]{reasoningSource});
+					
+					item0.setExpanded(false);
+					item1.setExpanded(true);
+				
+					int parentHeight = parent.getSize().y;
+					int compositeStart = reasoningResultResultComposite.getLocation().y;
+					item1.setHeight(parentHeight - compositeStart - item1.getHeaderHeight() - bar.getSpacing() - 4);
+					
+				} catch (CoreException ex) {
+					ex.printStackTrace();
+				}
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				//DO NOTHING
+			}
+		});
 		
 		//The view needs to register for listening to changes
 		try {
@@ -448,6 +522,9 @@ public class ReasoningView extends ViewPart implements IPropertyChangeListener, 
 			reasoningActionComboViewer.getCombo().select(0);
 			runButton.setEnabled(true);
 		}
+		
+		reasoningSelectionExpandItem.setExpanded(true);
+		reasoningResultExpandItem.setExpanded(false);
 	}
 	
 	Map<String, String> name2commandId = new HashMap<String, String>();
