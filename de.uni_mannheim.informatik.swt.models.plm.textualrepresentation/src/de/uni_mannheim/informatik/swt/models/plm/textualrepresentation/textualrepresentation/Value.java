775d5a9849904895d58342a16a12d723884d6a7a
ralphgerbig@gmail.com <ralphgerbig@gmail.com@7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1>
2012-09-11 15:10:00 +0000

    Extended textual visualization to navigate along connections which are not collections
    git-svn-id: https://svn.codespot.com/a/eclipselabs.org/melanie@1945 7cfea1e7-dead-e8d6-65ff-93ec3f6bf3c1

--- de.uni_mannheim.informatik.swt.models.plm.textualrepresentation/src/de/uni_mannheim/informatik/swt/models/plm/textualrepresentation/textualrepresentation/Value.java
+++ de.uni_mannheim.informatik.swt.models.plm.textualrepresentation/src/de/uni_mannheim/informatik/swt/models/plm/textualrepresentation/textualrepresentation/Value.java
@@ -22,7 +22,7 @@ public interface Value extends TextualVisualizationDescriptor {
 	 * Returns the textual visualization of the containing model element
 	 * <!-- end-model-doc -->
 	 * @model kind="operation" required="true"
-	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL body='self.getContainingPLMElement().oclAsType(PLM::Clabject).feature->select(f | f.oclIsKindOf(PLM::Attribute))->select(a | a.name = \'name\').oclAsType(PLM::Attribute).value->asOrderedSet()->first()'"
+	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL body='let attributeValue:String =\r\n\tlet attributes:OrderedSet(PLM::Attribute) = \r\n\t\tself.getContainingPLMElement().oclAsType(PLM::Clabject).feature->select(f | f.oclIsKindOf(PLM::Attribute))->select(a | a.name = expression).oclAsType(PLM::Attribute)->asOrderedSet()\r\n\tin\r\n\tif (attributes->size() > 0) then\r\n\t\tif (attributes.value->asOrderedSet()->first().oclIsUndefined()) then\r\n\t\t\t\'\'\r\n\t\telse\r\n\t\t\tattributes.value->asOrderedSet()->first()\r\n\t\tendif\r\n\telse \r\n\t\tnull\r\n\tendif\r\nin\r\nif (not attributeValue.oclIsUndefined()) then\r\n\tattributeValue\r\nelse\r\n\tself.getContainingPLMElement().oclAsType(PLM::Clabject).getDomainForRoleName(expression).visualizer.dslVisualizer->select(v | v.oclIsTypeOf(TextualDSLVisualizer)).oclAsType(TextualDSLVisualizer).createTextualVisualization()->first()\r\nendif'"
 	 * @generated
 	 */
 	String getValue();
--- de.uni_mannheim.informatik.swt.models.plm.textualrepresentation/src/de/uni_mannheim/informatik/swt/models/plm/textualrepresentation/textualrepresentation/impl/TextualrepresentationPackageImpl.java
+++ de.uni_mannheim.informatik.swt.models.plm.textualrepresentation/src/de/uni_mannheim/informatik/swt/models/plm/textualrepresentation/textualrepresentation/impl/TextualrepresentationPackageImpl.java
@@ -429,7 +429,7 @@ public class TextualrepresentationPackageImpl extends EPackageImpl implements Te
 		  (getValue__GetValue(), 
 		   source, 
 		   new String[] {
-			 "body", "self.getContainingPLMElement().oclAsType(PLM::Clabject).feature->select(f | f.oclIsKindOf(PLM::Attribute))->select(a | a.name = \'name\').oclAsType(PLM::Attribute).value->asOrderedSet()->first()"
+			 "body", "let attributeValue:String =\r\n\tlet attributes:OrderedSet(PLM::Attribute) = \r\n\t\tself.getContainingPLMElement().oclAsType(PLM::Clabject).feature->select(f | f.oclIsKindOf(PLM::Attribute))->select(a | a.name = expression).oclAsType(PLM::Attribute)->asOrderedSet()\r\n\tin\r\n\tif (attributes->size() > 0) then\r\n\t\tif (attributes.value->asOrderedSet()->first().oclIsUndefined()) then\r\n\t\t\t\'\'\r\n\t\telse\r\n\t\t\tattributes.value->asOrderedSet()->first()\r\n\t\tendif\r\n\telse \r\n\t\tnull\r\n\tendif\r\nin\r\nif (not attributeValue.oclIsUndefined()) then\r\n\tattributeValue\r\nelse\r\n\tself.getContainingPLMElement().oclAsType(PLM::Clabject).getDomainForRoleName(expression).visualizer.dslVisualizer->select(v | v.oclIsTypeOf(TextualDSLVisualizer)).oclAsType(TextualDSLVisualizer).createTextualVisualization()->first()\r\nendif"
 		   });	
 	}
 
