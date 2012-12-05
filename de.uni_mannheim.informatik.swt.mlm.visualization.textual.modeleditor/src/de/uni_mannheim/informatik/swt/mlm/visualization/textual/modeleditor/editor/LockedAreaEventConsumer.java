53b4cb77e5b49b235ffbae59f7d5047db5cc7ce1
Ralph Gerbig <ralphgerbig@gmail.com>
2012-12-05 09:38:17 +0100

    Removed never used class.

--- de.uni_mannheim.informatik.swt.mlm.visualization.textual.modeleditor/src/de/uni_mannheim/informatik/swt/mlm/visualization/textual/modeleditor/editor/LockedAreaEventConsumer.java
+++
@@ -1,7 +0,0 @@
-package de.uni_mannheim.informatik.swt.mlm.visualization.textual.modeleditor.editor;
-
-import org.eclipse.jface.text.IEventConsumer;
-
-public interface LockedAreaEventConsumer extends IEventConsumer {
-
-}
