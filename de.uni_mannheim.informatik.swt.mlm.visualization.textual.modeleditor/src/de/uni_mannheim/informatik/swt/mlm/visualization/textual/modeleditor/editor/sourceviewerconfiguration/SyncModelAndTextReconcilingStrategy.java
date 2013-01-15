6c17487ad5b24ebc48916288320d077f12172965
Ralph Gerbig <ralphgerbig@gmail.com>
2013-01-15 13:33:58 +0100

    Updated copyright statements.

--- de.uni_mannheim.informatik.swt.mlm.visualization.textual.modeleditor/src/de/uni_mannheim/informatik/swt/mlm/visualization/textual/modeleditor/editor/sourceviewerconfiguration/SyncModelAndTextReconcilingStrategy.java
+++ de.uni_mannheim.informatik.swt.mlm.visualization.textual.modeleditor/src/de/uni_mannheim/informatik/swt/mlm/visualization/textual/modeleditor/editor/sourceviewerconfiguration/SyncModelAndTextReconcilingStrategy.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2013 University of Mannheim: Chair for Software Engineering
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralph Gerbig - initial API and implementation and initial documentation
+ *******************************************************************************/
 package de.uni_mannheim.informatik.swt.mlm.visualization.textual.modeleditor.editor.sourceviewerconfiguration;
 
 import org.eclipse.core.runtime.IProgressMonitor;
