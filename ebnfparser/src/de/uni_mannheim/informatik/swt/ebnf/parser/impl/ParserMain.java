da6c1033d49ebe997c091d6a29bafb08e07ed8a7
Nikolai Hellwig <comavas@googlemail.com>
2013-04-18 11:52:23 +0200

    -- added generator model -- added first lexer implementation

--- ebnfparser/src/de/uni_mannheim/informatik/swt/ebnf/parser/impl/ParserMain.java
+++ ebnfparser/src/de/uni_mannheim/informatik/swt/ebnf/parser/impl/ParserMain.java
@@ -1,16 +1,35 @@
 package de.uni_mannheim.informatik.swt.ebnf.parser.impl;
 
+import java.io.FileInputStream;
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.Token;
+
+import de.uni_mannheim.informatik.swt.ebnf.parser.EBNFLexer;
+import de.uni_mannheim.informatik.swt.ebnf.parser.EBNFParser;
+
 public class ParserMain {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception { // TODO: EXCEPTION THROWING NEEDS TO BE REMOVED LATER
 		System.out.println("######################");
 		System.out.println("# EBNF TO EMF PARSER #");
 		System.out.println("######################");
 		
 		
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("test.ebnf"));
+		EBNFLexer lexer = new EBNFLexer(input);
+		
+		List<? extends Token> tokens = lexer.getAllTokens();
+		
+		for (Token t : tokens){
+			System.out.println("Type: " + EBNFLexer.tokenNames[t.getType()] + " Text: " + t.getText());
+		}
+		
+		//EBNFParser parser = new EBNFParser(lexer.get);
 	}
 
 }
