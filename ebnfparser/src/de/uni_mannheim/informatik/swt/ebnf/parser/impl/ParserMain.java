771d55169b7a32a4ab2f438cdf15ef1e99216e20
Nikolai Hellwig <comavas@googlemail.com>
2013-04-18 11:30:45 +0200

    - parser main java created - fixed ant for classpaths and testing with grun!!!

---
+++ ebnfparser/src/de/uni_mannheim/informatik/swt/ebnf/parser/impl/ParserMain.java
@@ -0,0 +1,16 @@
+package de.uni_mannheim.informatik.swt.ebnf.parser.impl;
+
+public class ParserMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("######################");
+		System.out.println("# EBNF TO EMF PARSER #");
+		System.out.println("######################");
+		
+		
+	}
+
+}
